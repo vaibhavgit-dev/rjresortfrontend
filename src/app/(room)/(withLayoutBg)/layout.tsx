b79df5e37a1c1ed074f8a5ec3e973b2ed0e62e0e
Gitvaibhavhibiscus <vaibhav@hibiscus.services>
2024-12-25 15:40:37 +0530

    layout header fixed

--- src/app/(room)/(withLayoutBg)/layout.tsx
+++ src/app/(room)/(withLayoutBg)/layout.tsx
@@ -1,13 +1,13 @@
 import BackToTop from "@/components/common/back-to-top";
 import FooterArea from "@/components/footer/footer-area";
 import MainProvider from "@/components/provider/main-provider";
-import HeaderOne from "@/components/header/header-aurum";
+import HeaderAurum from "@/components/header/header-aurum";
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <MainProvider>
-            <HeaderOne style_2={true}/>
+            <HeaderAurum />
             {children}
             <FooterArea/>
             <BackToTop/>
